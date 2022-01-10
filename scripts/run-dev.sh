ea02835b7c31893cddc1ae73690e153ea39744c4
Chris Taylor <forgetso86@gmail.com>
2022-01-10 17:10:10 +0000

    Fix make dev script

--- scripts/run-dev.sh
+++ scripts/run-dev.sh
@@ -5,30 +5,43 @@ docker compose up substrate-node -d
 echo "Waiting for the substrate node to start up..."
 sleep 10
 
+# install protocol packages
+$(cd protocol && yarn);
+
 # deploy contract and extract its address
-{ CONTRACT_ADDRESS=$(cd ./protocol && yarn && yarn deploy | tee /dev/fd/3 | grep 'contract address:' | awk -F ':  ' '{print $2}'); } 3>&1
+{ CONTRACT_ADDRESS=$(cd ./protocol && yarn deploy | tee /dev/fd/3 | grep 'contract address:' | awk -F ':  ' '{print $2}'); } 3>&1
 export CONTRACT_ADDRESS=$CONTRACT_ADDRESS
 
 # install packages
 $(cd provider && yarn);
-# generate mnemonic for the provider
 
+# generate mnemonic for the provider
 IFS=$'
 '
 { PROVIDER_KEYRING=($(cd ./provider && yarn mnemonic | tee /dev/fd/3 | grep ':' | awk -F ': ' '{print $2}' )); } 3>&1
 
-export PROVIDER_ADDRESS=${PROVIDER_KEYRING[1]}
-export PROVIDER_MNEMONIC=${PROVIDER_KEYRING[2]}
+export PROVIDER_ADDRESS=${PROVIDER_KEYRING[0]}
+export PROVIDER_MNEMONIC=${PROVIDER_KEYRING[1]}
 
-docker compose up mongodb -d
+# make sure the mnemonic is in the right format
+MNEMONIC_COUNT=$(echo ${PROVIDER_KEYRING[1]} | wc -w);
 
+if [[ $MNEMONIC_COUNT -ne 12 ]]; then
 
-echo "Dev env up! You can now interact with the provider-api."
-echo "PROVIDER_ADDRESS $(echo $PROVIDER_ADDRESS)"
-CONTAINER_NAME=$(docker ps -q -f name=provider-api)
+  echo "PROVIDER_MNEMONIC not set!"
+  exit 1
 
-# give the provider account some funds
-echo "Sending funds to the Provider account and registering the provider"
-docker exec -it $CONTAINER_NAME zsh -c 'yarn build && yarn setup provider'
+else
 
-docker exec -it $CONTAINER_NAME zsh
+  docker compose up mongodb -d
+  docker compose up provider-api -d
+
+  CONTAINER_NAME=$(docker ps -q -f name=provider-api)
+
+  # give the provider account some funds
+  echo "Sending funds to the Provider account and registering the provider"
+  docker exec -it $CONTAINER_NAME zsh -c 'yarn build && yarn setup provider'
+
+  echo "Dev env up! You can now interact with the provider-api."
+  docker exec -it $CONTAINER_NAME zsh
+fi
