ab79e2cc0fbb980b0c9fd74e845ccf3f81849d33
Chris Taylor <forgetso86@gmail.com>
2022-01-10 16:13:30 +0000

    Fix environment variables setter

--- scripts/run-dev.sh
+++ scripts/run-dev.sh
@@ -6,19 +6,22 @@ echo "Waiting for the substrate node to start up..."
 sleep 10
 
 # deploy contract and extract its address
-{ CONTRACT_ADDRESS=$(cd ./protocol && yarn deploy | tee /dev/fd/3 | grep 'contract address:' | awk -F ':  ' '{print $2}'); } 3>&1
+{ CONTRACT_ADDRESS=$(cd ./protocol && yarn && yarn deploy | tee /dev/fd/3 | grep 'contract address:' | awk -F ':  ' '{print $2}'); } 3>&1
 export CONTRACT_ADDRESS=$CONTRACT_ADDRESS
 
+# install packages
+$(cd provider && yarn);
 # generate mnemonic for the provider
+
 IFS=$'
 '
 { PROVIDER_KEYRING=($(cd ./provider && yarn mnemonic | tee /dev/fd/3 | grep ':' | awk -F ': ' '{print $2}' )); } 3>&1
 
-export PROVIDER_ADDRESS=${PROVIDER_KEYRING[0]}
-export PROVIDER_MNEMONIC=${PROVIDER_KEYRING[1]}
+export PROVIDER_ADDRESS=${PROVIDER_KEYRING[1]}
+export PROVIDER_MNEMONIC=${PROVIDER_KEYRING[2]}
 
 docker compose up mongodb -d
-docker compose up provider-api -d
+
 
 echo "Dev env up! You can now interact with the provider-api."
 echo "PROVIDER_ADDRESS $(echo $PROVIDER_ADDRESS)"
