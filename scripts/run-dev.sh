0e787e7ca5fb517f781688bf64e0622f001f2e48
Chris Taylor <forgetso86@gmail.com>
2022-01-13 16:16:27 +0000

    Move mnemonic generation into protocol

--- scripts/run-dev.sh
+++ scripts/run-dev.sh
@@ -16,13 +16,11 @@ export DAPP_CONTRACT_ADDRESS=$DAPP_CONTRACT_ADDRESS
 { CONTRACT_ADDRESS=$(cd ./protocol && yarn deploy | tee /dev/fd/3 | grep 'contract address:' | awk -F ':  ' '{print $2}'); } 3>&1
 export CONTRACT_ADDRESS=$CONTRACT_ADDRESS
 
-# install packages
-$(cd provider && yarn);
-
-# generate mnemonic for the provider
+# generate a mnemonic for the provider
 IFS=$'
 '
-{ PROVIDER_KEYRING=($(cd ./provider && yarn mnemonic | tee /dev/fd/3 | grep ':' | awk -F ': ' '{print $2}' )); } 3>&1
+yarn
+{ PROVIDER_KEYRING=($(cd ./protocol && yarn mnemonic | tee /dev/fd/3 | grep ':' | awk -F ': ' '{print $2}' )); } 3>&1
 
 export PROVIDER_ADDRESS=${PROVIDER_KEYRING[0]}
 export PROVIDER_MNEMONIC=${PROVIDER_KEYRING[1]}
@@ -33,19 +31,21 @@ MNEMONIC_COUNT=$(echo ${PROVIDER_KEYRING[1]} | wc -w);
 if [[ $MNEMONIC_COUNT -ne 12 ]]; then
 
   echo "PROVIDER_MNEMONIC not set!"
+  echo "Provider keyring index 0:  $PROVIDER_KEYRING[0]"
+  echo "Provider keyring index 1:  $PROVIDER_KEYRING[1]"
+  echo "Provider keyring index 2:  $PROVIDER_KEYRING[2]"
   exit 1
-
 else
-
-  docker compose up mongodb -d
-  docker compose up provider-api -d
-
-  CONTAINER_NAME=$(docker ps -q -f name=provider-api)
-
-  # give the provider account some funds
-  echo "Sending funds to the Provider account and registering the provider"
-  docker exec -it $CONTAINER_NAME zsh -c 'yarn build && yarn setup provider'
-
-  echo "Dev env up! You can now interact with the provider-api."
-  docker exec -it $CONTAINER_NAME zsh
+  echo "PROVIDER_MNEMONIC set"
 fi
+
+docker compose up mongodb -d
+docker compose up provider-api -d
+
+CONTAINER_NAME=$(docker ps -q -f name=provider-api)
+
+echo "Sending funds to the Provider account and registering the provider"
+docker exec -it $CONTAINER_NAME zsh -c 'yarn && yarn build && yarn setup provider && yarn setup dapp'
+
+echo "Dev env up! You can now interact with the provider-api."
+docker exec -it $CONTAINER_NAME zsh
