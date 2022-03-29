c970926abdbc11ea296fc0de8da145d353273975
Chris Taylor <forgetso86@gmail.com>
2022-03-29 21:27:16 +0100

    Pull new milestone1 version of redspot

--- scripts/setup-dev.sh
+++ scripts/setup-dev.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 git submodule update --init --recursive --force --checkout
 git checkout milestone1 --recurse-submodules
-$(cd protocol && git checkout milestone1)
-$(cd dapp-example && git checkout milestone1)
-$(cd provider && git checkout milestone1)
+cd protocol && git checkout milestone1 && cd ..
+cd dapp-example && git checkout milestone1 && cd ..
+cd provider && git checkout milestone1 && cd ..
+cd redspot && git checkout milestone1 && cd ..
