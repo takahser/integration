0bdd48512dfb60be2d732a9d4a9a7f23e63a936b
Chris Taylor <forgetso86@gmail.com>
2022-04-13 15:30:46 +0100

    hotfix: set yarn version to stable

--- docker/dev.dockerfile.upgrade.yarn.sh
+++ docker/dev.dockerfile.upgrade.yarn.sh
@@ -1,4 +1,4 @@
 YARN_VERSION=$(yarn --version) && echo "$YARN_VERSION"
 if [[ ${YARN_VERSION:0:1} == "1" ]]; then
-  echo "Upgrading yarn" && yarn set version latest && yarn plugin import workspace-tools
+  echo "Upgrading yarn" && yarn set version stable && yarn plugin import workspace-tools
 fi
